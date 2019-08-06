f4d54cfab46867b4ad758faeb230fed3cf270f56
Yoshikomaru <yoshikomaru195@gmail.com>
2019-08-06 18:33:02 +0300

    Добавлена кнопка 'Вверх'

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -9,3 +9,17 @@ $('#offer-form').on('submit', function(event) {
     }
   })
 });
+
+var top_show = 150;
+var delay = 1000;
+$(document).ready(function() {
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > top_show) $('#top').fadeIn();
+    else $('#top').fadeOut();
+  });
+  $('#top').click(function () {
+    $('body, html').animate({
+      scrollTop: 0
+    }, delay);
+  });
+});
