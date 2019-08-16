1fc2f22a345e50070f0da42feadf994dac9fe627
Yoshikomaru <yoshikomaru195@gmail.com>
2019-08-16 13:38:11 +0300

    Оптимизация

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,18 +1,17 @@
-$('#offer-form').on('submit', function(event) {
-  event.preventDefault();
-  $.ajax({
-    url: 'mail.php',
-    type: 'post',
-    data: $(this).serialize(),
-    success: function(data) {
-      $('.success').html(data + ', ваша форма отправлена.')
-    }
-  })
-});
-
 var top_show = 150;
 var delay = 1000;
 $(document).ready(function() {
+  $('#offer-form').on('submit', function(event) {
+    event.preventDefault();
+    $.ajax({
+      url: 'mail.php',
+      type: 'post',
+      data: $(this).serialize(),
+      success: function(data) {
+        $('.success').html(data + ', ваша форма отправлена.')
+      }
+    })
+  });
   $(window).scroll(function () {
     if ($(this).scrollTop() > top_show) $('#top').fadeIn();
     else $('#top').fadeOut();
