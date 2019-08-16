c1e2879c6e9a1c78cf3fef17c6c733514de6d466
Yoshikomaru <yoshikomaru195@gmail.com>
2019-08-16 12:43:03 +0300

    Оптимизация сайта

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,47 @@
-function defaultTask(cb) {
-  
-  cb();
-}
+var gulp = require('gulp');
+var cleanCSS = require('gulp-clean-css');
+var htmlmin = require('gulp-htmlmin');
+var tinyPNG = require('gulp-tinypng-compress');
 
-exports.default = defaultTask
+gulp.task('minify-css', function(done) {
+  return gulp.src('./src/css/*.css')
+  .pipe(cleanCSS({
+    compatibility: 'ie8'
+  }))
+  .pipe(gulp.dest('dist/css/'))
+  done();
+});
+
+gulp.task('move-js', function(done) {
+  return gulp.src('./src/js/*.js')
+  .pipe(gulp.dest('dist/js/'))
+  done();
+});
+
+gulp.task('htmlmin', function(done) {
+  return gulp.src('./src/*.html')
+  .pipe(htmlmin({ 
+    collapseWhitespace: true 
+  }))
+  .pipe(gulp.dest('dist/'))
+  done();
+});
+
+gulp.task('fonts', function(done) {
+  return gulp.src('./src/fonts/**/*')
+  .pipe(gulp.dest('dist/fonts'))
+  done();
+});
+
+gulp.task('tinypng', function(done) {
+  gulp.src('./src/img/**/*.{png,jpg,jpeg}')
+      .pipe(tinyPNG({
+          key: 'YRn4GJmpKvDrcYf6fFmbygXvsW27yTSr',
+      }))
+      .pipe(gulp.dest('dist/img/'));
+      done();
+});
+
+gulp.task('default', gulp.parallel('minify-css', 'move-js', 'fonts', 'htmlmin', 'tinypng', function (done) {
+  done();
+}))
